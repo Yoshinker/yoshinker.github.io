5061d307e25ac2c994e0f05f355cfcda369f0d01
yoshinker <pc.tom74600@gmail.com>
2019-08-02 03:11:13 +0200

    init first website

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<h1>Hello world !</h1>
+
+	<ul>
+		<li><a href="web1">Web1</a></li>
+	</ul>
+
+</body>
+</html>
---
+++ web1/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Apple 2</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="css/default.css">
+	<link rel="stylesheet" type="text/css" href="/libs/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/libs/css/fontawesome-all.css">
+</head>
+<body>
+
+	<header></header>
+
+	<div id="content">
+		
+		<div class="row">
+			<div class="col-sm-4">OUI</div>
+			<div class="col-sm-4">NON</div>
+			<div class="col-sm-4">HUMMM</div>
+		</div>
+
+	</div>
+
+	<footer>
+		
+	</footer>
+
+	<!-- Bootstrap JS -->
+	<script src="/libs/js/jquery-3.4.1.min.js"></script>
+	<script src="/libs/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
