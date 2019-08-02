183776bfefa4eb79614278531c36e5b790a9a981
yoshinker <pc.tom74600@gmail.com>
2019-08-02 03:15:18 +0200

    add robots.txt

--- web1/index.php
+++ web1/index.php
@@ -5,8 +5,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" type="text/css" href="css/default.css">
-	<link rel="stylesheet" type="text/css" href="/libs/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="/libs/css/fontawesome-all.css">
+	<link rel="stylesheet" type="text/css" href="../libs/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../libs/css/fontawesome-all.css">
 </head>
 <body>
 
@@ -27,8 +27,8 @@
 	</footer>
 
 	<!-- Bootstrap JS -->
-	<script src="/libs/js/jquery-3.4.1.min.js"></script>
-	<script src="/libs/js/bootstrap.bundle.min.js"></script>
+	<script src="../libs/js/jquery-3.4.1.min.js"></script>
+	<script src="../libs/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
